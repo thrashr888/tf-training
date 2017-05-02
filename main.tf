1252515012274e81764b758b6ea5471c8a90da56
Paul Thrasher <pthrasher@hashicorp.com>
2017-05-01 19:24:07 -0700

    fix remote and vpc

--- main.tf
+++ main.tf
@@ -1,15 +1,23 @@
+
+terraform {
+  backend "atlas" {
+    name    = "pthrasher/training"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "web" {
-  ami="ami-c58c1dd3"
-  instance_type = "t2.micro"
-  security_groups = ["sg-ac29cfc5"]
-  key_name = "vastermonster"
+  ami             = "ami-c58c1dd3"
+  instance_type   = "t2.micro"
+  security_groups = ["sg-e38c9b87"]
+  key_name        = "vastermonster"
+  subnet_id       = "subnet-1523252f"
 
   tags {
-    Name = "Hello World"
+    Name  = "Hello World"
     Owner = "Terraform"
   }
 }
